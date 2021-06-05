8a583ef5e1c3e2845a93e87b11de00b8a5079644
Fahmi <mohamed.a.fahmi.99@gmail.com>
2021-06-06 01:11:37 +0200

    fahmi_edit

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 #2 FOR RED CHIP
 #3 FOR YELLOW CHIP
 
-def state_children(state,val):
+def state_children(state, val):
     children = []
     for i in range(0,7):
         for j in range(0,6):
             if get_chip(state[i],j) == 1:
-                child = set_chip(state,i,j,val)
+                child = set_chip(state, i, j, val)
                 children.append(child)
                 break
     return children
@@ -47,11 +47,46 @@ def horizontal_count(state, val):
             count += 1
     return count
 
+def diagonal_count(state, val):
+    count = 0
+    for k in range(0,6+7-2+1):
+        fourinrow = 4
+        for j in range(0,k+1):
+            i = k - j
+            if i<6 and j<7:
+                if get_chip(state[j], i) == val:
+                    #print(get_chip(state[j],i))
+                    fourinrow -= 1
+                    if fourinrow == 0:
+                        count += 1
+                else:
+                    fourinrow = 4
 
-#
-# def red_score(state):
-#
-# def yellow_score(state):
+    # for k in range(0,12):
+    #     fourinrow = 4
+    #     for i in range(0,k+1):
+    #         j = 6 + i
+    #         if i > -1 and j < 7:
+    #             print(i, j)
+    #             if get_chip(state[j], i) == val:
+    #                 #print(get_chip(state[j],i))
+    #
+    #                 fourinrow -= 1
+    #                 if fourinrow == 0:
+    #                     count += 1
+    #             else:
+    #                 fourinrow = 4
+    #     print('\n')
+    return count
+
+
+def red_score(state):
+    return horizontal_count(state, 2) + vertical_count(state, 2) + diagonal_count(state, 2)
+
+
+def yellow_score(state):
+    return horizontal_count(state, 3) + vertical_count(state, 3) + diagonal_count(state, 3)
+
 
 def get_chip(column, j):
     return int(column/(10**(5-j))%10)
@@ -79,13 +114,15 @@ def set_chip(state, i , j, val):
    return new_state
 
 
-state = [222211, 222211, 211111, 221111, 322221, 321111, 321111]
+state = [222221, 222211, 232111, 221111, 322221, 322111, 323211]
 
 #state = ['000200','000000','000000','000000','000000','000000','000000']
 
 list1 = state_children(state,3)
-print(horizontal_count(state, 2))
-print(vertical_count(state, 2))
+print(red_score(state))
+print(yellow_score(state))
+#print(horizontal_count(state, 2))
+#print(vertical_count(state, 2))
 for j in range(5,-1,-1):
     print(get_chip(state[0],j),get_chip(state[1],j),get_chip(state[2],j),get_chip(state[3],j),get_chip(state[4],j),get_chip(state[5],j),get_chip(state[6],j))
 
