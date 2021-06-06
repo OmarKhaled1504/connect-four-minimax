50da8f152148eadb0a839d12d38d990fc5ac1125
Fahmi <mohamed.a.fahmi.99@gmail.com>
2021-06-06 02:27:04 +0200

    fahmi_edit

--- main.py
+++ main.py
@@ -87,6 +87,41 @@ def red_score(state):
 def yellow_score(state):
     return horizontal_count(state, 3) + vertical_count(state, 3) + diagonal_count(state, 3)
 
+def evaluate(state):
+    return red_score(state)-yellow_score(state)
+
+def terminal_test(state):
+    for i in range(0, 7):
+        for j in range(0, 6):
+            if get_chip(state[i], j) == 1:
+                return 0
+    return 1
+
+def maximize(state):
+    if terminal_test(state):
+        return (None, evaluate(state))
+    (maxChild, maxUtility) = (None, -500)
+    for child in state_children(state,2):
+        (temp, utility) = minimize(child)
+        if utility > maxUtility:
+            (maxChild,maxUility) = (child, utility)
+
+    return (maxChild, maxUtility)
+
+def minimize(state):
+    if terminal_test(state):
+        return (None, evaluate(state))
+    (minChild, minUtility) = (None, 500)
+    for child in state_children(state,3):
+        (temp, utility) = maximize(child)
+        if utility < minUtility:
+            (minChild, minUility) = (child, utility)
+
+    return (minChild, minUtility)
+
+def decision(state):
+    (child,temp) = maximize(state)
+    return child
 
 def get_chip(column, j):
     return int(column/(10**(5-j))%10)
@@ -109,23 +144,36 @@ def set_chip(state, i , j, val):
    x = split(str(new_state[i]))
    x[j] = str(val)
    new_state[i] = int(listToString(x))
-   #print(new_state)
-   #print('\n')
    return new_state
 
+def print_board(state):
+    for j in range(5, -1, -1):
+        print(get_chip(state[0], j), get_chip(state[1], j), get_chip(state[2], j), get_chip(state[3], j),
+              get_chip(state[4], j), get_chip(state[5], j), get_chip(state[6], j))
 
-state = [222221, 222211, 232111, 221111, 322221, 322111, 323211]
+state = [222221, 222231, 232331, 223311, 322221, 322331, 323231]
 
 #state = ['000200','000000','000000','000000','000000','000000','000000']
 
 list1 = state_children(state,3)
 print(red_score(state))
 print(yellow_score(state))
-#print(horizontal_count(state, 2))
-#print(vertical_count(state, 2))
-for j in range(5,-1,-1):
-    print(get_chip(state[0],j),get_chip(state[1],j),get_chip(state[2],j),get_chip(state[3],j),get_chip(state[4],j),get_chip(state[5],j),get_chip(state[6],j))
 
-
-
+print_board(state)
+print('\n')
+state = decision(state)
+print_board(state)
+print('\n')
+state = decision(state)
+print_board(state)
+print('\n')
+state = decision(state)
+print_board(state)
+print('\n')
+state = decision(state)
+print_board(state)
+print('\n')
+state = decision(state)
+print_board(state)
+print('\n')
 
